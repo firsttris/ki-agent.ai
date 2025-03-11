1831fec9d03f91977485d82dfd6418ba255d9ba4
Tristan Teufel <tristanteufel@googlemail.com>
2025-03-11 10:05:05 +0100

    update canocial url meta data for search console

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import sitemap from '@astrojs/sitemap';
 
 import icon from 'astro-icon';
 
 export default defineConfig({
   site: 'https://www.ki-agent.ai',
-  integrations: [sitemap(), icon()],
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
   }
